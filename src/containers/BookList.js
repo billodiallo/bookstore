38476c83bc70c6e3abe30eca1c9bcaa3cf172979
Billo Diallo <billodiallonet@gmail.com>
2021-06-29 16:45:13 +0300

    Add BookList function and jsx

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -2,3 +2,19 @@ import React from "react";
 import {connect} from "react-redux";
 import { ProtoTypes } from "prop-types";
 import Book from "../components/Book";
+
+const BookList = props => {
+    const {books} = props;
+    return (
+        <div>
+            <h1>Books as html table</h1>
+            <table>
+                <tr>
+                    <th>Book ID</th>
+                    <th>Title</th>
+                    <th>category</th>
+                </tr>
+            </table>
+        </div>
+    )
+}
