f5ab71287e7ead292d5f6ecaca3955704baf5f8a
Billo Diallo <billodiallonet@gmail.com>
2021-06-29 15:57:39 +0300

    Add prototype and default values

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import ProtoType from "prop-types";
 
-const Book = props => {
-    const {id, title, category} = props;
-
-    return(
-        <tr>
-            <th>{id}</th>
-            <th>{title}</th>
-            <th>{category}</th>
-
-        </tr>
+const Books = props => {
+    const { id, title, category } = props;
+    return (
+      <tr>
+        <th>{id}</th>
+        <th>{title}</th>
+        <th>{category}</th>
+      </tr>
     );
+  };
+
+Books.prototype = {
+    id: ProtoType.number,
+    title: ProtoType.string,
+    category: ProtoType.string
 };
 
-Book.prototype = {
-    id.ProtoType.number,
-    title.ProtoType.string,
-    category.ProtoType.string
+Books.default = {
+    id: 99,
+    title: "title must fill here",
+    category: "category must here",
+    
 };
+
+export default Books;
