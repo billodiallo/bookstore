521a2d7051223813b5964bf99b20c38267107b0d
Billo Diallo <billodiallonet@gmail.com>
2021-06-29 15:48:59 +0300

    Add validations

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,2 +1,21 @@
 import React from 'react';
 import ProtoType from "prop-types";
+
+const Book = props => {
+    const {id, title, category} = props;
+
+    return(
+        <tr>
+            <th>{id}</th>
+            <th>{title}</th>
+            <th>{category}</th>
+
+        </tr>
+    );
+};
+
+Book.prototype = {
+    id.ProtoType.number,
+    title.ProtoType.string,
+    category.ProtoType.string
+};
