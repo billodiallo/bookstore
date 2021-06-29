3c93b9fff3867bc089983551fe887eaa5e6d462f
Billo Diallo <billodiallonet@gmail.com>
2021-06-29 18:41:35 +0300

    Add BookForm, BookCategories

---
+++ src/containers/BookForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const BookForm = () => {
+    const BookCategories = [
+        'Action',
+        'Biography',
+        'History',
+        'Horror',
+        'Kids',
+        'Learning',
+        'Sci-Fi',
+    ];
+
+
+    return (
+        <div>
+            <form>
+                <label htmlFor= "title">
+                    
+                </label>
+            </form>
+        </div>
+    )
+}
