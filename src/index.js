a091e1c67aa75afa51e4a3a23877d2ec50fad636
Billo Diallo <billodiallonet@gmail.com>
2021-06-30 11:02:36 +0300

    Add storeand wrapped Provider

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import rootReducer from './reducers';
 
+const store = createStore(rootReducer)
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store} >
     <App />
+    </Provider>
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
