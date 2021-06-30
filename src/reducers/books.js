45f62c8e7eeaa888b2904d027e301796d51a9faa
Billo Diallo <billodiallonet@gmail.com>
2021-06-30 09:22:14 +0300

    Add books function and first action type

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,2 +1,11 @@
 import { CREATE_BOOK, REMOVE_BOOK } from "../constaints/action-types";
 import {books as booksArrays} from '../static.data'
+
+export default function books(store = booksArrays,action) {
+if (action.type == CREATE_BOOK) {
+    return [
+        ...store, action.payload,
+    ]
+}
+
+}
