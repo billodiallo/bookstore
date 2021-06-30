cefd7e077a1b1ec9257246fabc087fe699bb161a
Billo Diallo <billodiallonet@gmail.com>
2021-06-30 14:07:21 +0300

    Fix bugs

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,5 +1,5 @@
 import { CREATE_BOOK, REMOVE_BOOK } from "../constaints/action-types";
-import {books as booksArrays} from '../static.data'
+import {books as booksArrays} from '../static-data'
 
 export default function books(store = booksArrays,action) {
 if (action.type == CREATE_BOOK) {
