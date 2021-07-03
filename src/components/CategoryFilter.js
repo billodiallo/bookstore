52f0980891322388fdd4b9c62ef6386055a7423a
Billo Diallo <billodiallonet@gmail.com>
2021-07-03 16:22:53 +0300

    Declare handleChange, bookCategoriesExtended

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import PropType from 'prop-types'
+import bookcategories from '../constaints/bookCategories';
+const CategoryFilter = props => {
+    const {handleChange} = props;
+    const bookCategoresExtented = ['All', ...bookcategories];
+    
+
+}
