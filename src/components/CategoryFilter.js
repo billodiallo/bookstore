9e16d6e54ff3a57a9b48c7188bb5420f06c5f8ce
Billo Diallo <billodiallonet@gmail.com>
2021-07-03 16:38:43 +0300

    bookcategories form

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -4,6 +4,22 @@ import bookcategories from '../constaints/bookCategories';
 const CategoryFilter = props => {
     const {handleChange} = props;
     const bookCategoresExtented = ['All', ...bookcategories];
-    
+    return (
+        <label htmlFor = 'category'>
+            Category:
+            <select onChange= {handleChange}
+            name = 'category'>
+               {bookCategoriesExtended.map(category => (
+          <option
+            key={category}
+            value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
 
-}
+        </label>
+
+    );
+
+};
