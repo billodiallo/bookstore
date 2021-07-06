46336c528124ab919e2c01d3c61b70fcf92fe690
Billo Diallo <billodiallonet@gmail.com>
2021-07-06 16:29:11 +0300

    Add completedPercent, currentChapter,Comment,Remove,Update

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,17 +4,31 @@ import ProtoType from 'prop-types';
 
 const Books = (props) => {
   const {
-    id, title, category, handleClick,
+    id, title, category,completedPercent,currentChapeter, handleClick,
   } = props;
   return (
-    <tr>
-      <th>{id}</th>
-      <th>{title}</th>
-      <th>{category}</th>
-      <button type="button" id={id} onClick={handleClick}>X</button>
-    </tr>
-  );
-};
+    <ul className ="book-ul"> 
+  <li>
+  <div className = "caterory-title-author">
+    <p className = "category black-2">{category}</p>
+    <p className = "title">{title}</p>
+    <a href = "#" id = {id}> author</a> 
+
+  <a href = "#" id = {id} className = 'flex-row-item Line-2 padding-1'>
+    Comment
+  </a>
+
+  <a href = "#" id = {id}  onClick= {handleClick} className = "flex-row-item Line-2 padding-1">
+Remove
+  </a>
+
+  <a href = "#" id = {id} className = "flex-row-item Line-2 padding-1">
+    Edit
+  </a>
+  </div>
+  </li>
+    </ul>
+    
 
 Books.prototype = {
   id: ProtoType.number,
